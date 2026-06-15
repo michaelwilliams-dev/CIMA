65604b184ec4a783da887f7360055acfef8627d6
michaelwilliams-dev <michael@aivs.uk>
2026-06-15 16:45:35 +0100

    Update server.js

--- server.js
+++ server.js
@@ -138,7 +138,16 @@ app.use(cors());
 app.options("*", cors());
 app.use(express.json({ limit: "5mb" }));
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "public"), {
+  etag: false,
+  lastModified: false,
+  setHeaders: (res) => {
+    res.setHeader("Cache-Control", "no-store, no-cache, must-revalidate, proxy-revalidate");
+    res.setHeader("Pragma", "no-cache");
+    res.setHeader("Expires", "0");
+    res.setHeader("Surrogate-Control", "no-store");
+  }
+}));
 
 registerTrainingSynopsisRoute(app, {
   BUILD_ISO,
